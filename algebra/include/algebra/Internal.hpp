41de9a5cd6c5b852439391db32ccd08c3531c587
Loïc Corenthy <loic.corenthy@gmail.com>
2022-08-08 07:45:43 +0100

    WIP: Add previous implementation of Vector

---
+++ algebra/include/algebra/Internal.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace LCNS::Algebra
+{
+    /*!
+     *  \brief Internal type to define vector of dimension 1
+     */
+    struct One
+    {
+    }; // struct One
+
+    /*!
+     *  \brief internal type to define vector of dimension 2
+     */
+    struct Two
+    {
+    }; // struct Two
+
+    /*!
+     *  \brief internal type to define vector of dimension 3
+     */
+    struct Three
+    {
+    }; // struct Three
+
+    /*!
+     *  \brief internal type to define vector of dimension 4
+     */
+    struct Four
+    {
+    }; // struct Four
+
+    /*!
+     *  \brief internal type to define vector of any dimension
+     */
+    struct AnyDimension
+    {
+    }; // struct AnyDimension
+}
