ed7f25fae9663500f9ab2f4dcad4a56f4a7cb345
Loïc Corenthy <loic.corenthy@gmail.com>
2022-08-21 11:32:53 +0100

    Add short type for vectors of size 1

--- algebra/include/algebra/Algebra.hpp
+++ algebra/include/algebra/Algebra.hpp
@@ -2,6 +2,11 @@
 
 #include "algebra/Vector.hpp"
 
+using vec1i = LCNS::Algebra::Vector<int, 1>;
+using vec1u = LCNS::Algebra::Vector<unsigned int, 1>;
+using vec1f = LCNS::Algebra::Vector<float, 1>;
+using vec1d = LCNS::Algebra::Vector<double, 1>;
+
 using vec2i = LCNS::Algebra::Vector<int, 2>;
 using vec2u = LCNS::Algebra::Vector<unsigned int, 2>;
 using vec2f = LCNS::Algebra::Vector<float, 2>;
