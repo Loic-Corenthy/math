347f2156760c7afbffe05e9a661ddf9d260fc627
Loïc Corenthy <loic.corenthy@gmail.com>
2022-08-21 07:49:48 +0100

    Add include defining most commonly used vector types

---
+++ algebra/include/algebra/Algebra.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "algebra/Vector.hpp"
+
+using vec2i = LCNS::Algebra::Vector<int, 2>;
+using vec2u = LCNS::Algebra::Vector<unsigned int, 2>;
+using vec2f = LCNS::Algebra::Vector<float, 2>;
+using vec2d = LCNS::Algebra::Vector<double, 2>;
+
+using vec3i = LCNS::Algebra::Vector<int, 3>;
+using vec3u = LCNS::Algebra::Vector<unsigned int, 3>;
+using vec3f = LCNS::Algebra::Vector<float, 3>;
+using vec3d = LCNS::Algebra::Vector<double, 3>;
+
+using vec4i = LCNS::Algebra::Vector<int, 4>;
+using vec4u = LCNS::Algebra::Vector<unsigned int, 4>;
+using vec4f = LCNS::Algebra::Vector<float, 4>;
+using vec4d = LCNS::Algebra::Vector<double, 4>;
