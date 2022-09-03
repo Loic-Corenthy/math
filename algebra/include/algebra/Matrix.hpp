7eb0601bd82f20113069dde7edd802059a56dbaa
Loïc Corenthy <loic.corenthy@gmail.com>
2022-09-03 09:02:25 +0100

    Store coefficients linearly using std::array

--- algebra/include/algebra/Matrix.hpp
+++ algebra/include/algebra/Matrix.hpp
@@ -31,22 +31,31 @@ namespace LCNS::Algebra
 
         /*!
          * \brief Accessor (read/write)
-         * @param row in the index of the row where to find the coefficient to access
-         * @param col in the index of the column where to find the coefficient to access
+         * @param i is the index of the row where to find the coefficient to access
+         * @param j is the index of the column where to find the coefficient to access
          * @return a reference on the corresponding coefficient
          */
-        constexpr coordinate& operator()(unsigned int row, unsigned int col);
+        coordinate& operator()(unsigned int i, unsigned int j);
 
         /*!
          * \brief Accessor (read only)
-         * @param row in the index of the row where to find the coefficient to access
-         * @param col in the index of the column where to find the coefficient to access
+         * @param i is the index of the row where to find the coefficient to access
+         * @param j is the index of the column where to find the coefficient to access
          * @return the corresponding coefficient
          */
-        constexpr coordinate operator()(unsigned int row, unsigned int col) const;
+        constexpr coordinate operator()(unsigned int i, unsigned int j) const;
 
     private:
-        coordinate _coeff[rows][cols] = {};
+        /*!
+         * \brief Unnamed helper function to get the 1D equalent of i and j
+         * @param i is the index of the row where to find the coefficient to access
+         * @param j is the index of the column where to find the coefficient to access
+         * @return the equivalent index to access the coefficient in _coeff
+         */
+        constexpr unsigned int _(unsigned int i, unsigned int j) const;
+
+    private:
+        std::array<coordinate, rows* cols> _coeff = {};
 
     };  // class Matrix
 
@@ -57,36 +66,34 @@ namespace LCNS::Algebra
 
         for (unsigned int i = 0; i < rows; ++i)
             for (unsigned int j = 0; j < cols; ++j)
-                _coeff[i][j] = (i == j) ? scalar : static_cast<coordinate>(0);
+                _coeff[_(i, j)] = (i == j) ? scalar : static_cast<coordinate>(0);
     }
 
     template <Coordinate coordinate, unsigned int rows, unsigned int cols>
     constexpr Matrix<coordinate, rows, cols>::Matrix(const std::initializer_list<coordinate>& coeffs)
     {
         unsigned int i = 0;
-        unsigned int j = 0;
-
         for (auto it = coeffs.begin(); it < coeffs.end(); it++)
         {
-            _coeff[i][j] = *it;
-            j++;
-            if (j == cols)
-            {
-                i++;
-                j = 0;
-            }
+            _coeff[i++] = *it;
         }
     }
 
     template <Coordinate coordinate, unsigned int rows, unsigned int cols>
-    constexpr coordinate& Matrix<coordinate, rows, cols>::operator()(unsigned int row, unsigned int col)
+    coordinate& Matrix<coordinate, rows, cols>::operator()(unsigned int i, unsigned int j)
     {
-        return _coeff[row][col];
+        return _coeff[_(i, j)];
     }
 
     template <Coordinate coordinate, unsigned int rows, unsigned int cols>
-    constexpr coordinate Matrix<coordinate, rows, cols>::operator()(unsigned int row, unsigned int col) const
+    constexpr coordinate Matrix<coordinate, rows, cols>::operator()(unsigned int i, unsigned int j) const
     {
-        return _coeff[row][col];
+        return _coeff[_(i, j)];
+    }
+
+    template <Coordinate coordinate, unsigned int rows, unsigned int cols>
+    inline constexpr unsigned int Matrix<coordinate, rows, cols>::_(unsigned int i, unsigned int j) const
+    {
+        return i * cols + j;
     }
 }  // namespace LCNS::Algebra
