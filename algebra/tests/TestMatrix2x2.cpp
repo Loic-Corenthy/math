cce9144ea217cfb9552761a3fe78bd2397ce23cc
Loïc Corenthy <loic.corenthy@gmail.com>
2022-09-03 19:43:15 +0100

    Change naming convention for matrix coefficients in tests

--- algebra/tests/TestMatrix2x2.cpp
+++ algebra/tests/TestMatrix2x2.cpp
@@ -11,64 +11,64 @@ using FloatingTypes = std::tuple<float, double, long double>;
 
 TEMPLATE_LIST_TEST_CASE("Accessor operator", "[algebra][matrix][dim2][operator]", IntegerTypes)
 {
-    constexpr TestType x1 = -1;
-    constexpr TestType y1 = 123;
-    constexpr TestType x2 = 19;
-    constexpr TestType y2 = -10;
+    constexpr TestType m00 = -1;
+    constexpr TestType m01 = 123;
+    constexpr TestType m10 = 19;
+    constexpr TestType m11 = -10;
 
-    constexpr Matrix<TestType, 2, 2> mat1 = { x1, y1, x2, y2 };
+    constexpr Matrix<TestType, 2, 2> mat1 = { m00, m01, m10, m11 };
 
-    static_assert(mat1(0, 0) == x1);
-    static_assert(mat1(0, 1) == y1);
-    static_assert(mat1(1, 0) == x2);
-    static_assert(mat1(1, 1) == y2);
+    static_assert(mat1(0, 0) == m00);
+    static_assert(mat1(0, 1) == m01);
+    static_assert(mat1(1, 0) == m10);
+    static_assert(mat1(1, 1) == m11);
 
-    CHECK(mat1(0, 0) == x1);
-    CHECK(mat1(0, 1) == y1);
-    CHECK(mat1(1, 0) == x2);
-    CHECK(mat1(1, 1) == y2);
+    CHECK(mat1(0, 0) == m00);
+    CHECK(mat1(0, 1) == m01);
+    CHECK(mat1(1, 0) == m10);
+    CHECK(mat1(1, 1) == m11);
 
     Matrix<TestType, 2, 2> mat2;
-    mat2(0, 0) = y2;
-    mat2(0, 1) = x1;
-    mat2(1, 0) = y1;
-    mat2(1, 1) = x2;
+    mat2(0, 0) = m11;
+    mat2(0, 1) = m00;
+    mat2(1, 0) = m01;
+    mat2(1, 1) = m10;
 
-    CHECK(mat2(0, 0) == y2);
-    CHECK(mat2(0, 1) == x1);
-    CHECK(mat2(1, 0) == y1);
-    CHECK(mat2(1, 1) == x2);
+    CHECK(mat2(0, 0) == m11);
+    CHECK(mat2(0, 1) == m00);
+    CHECK(mat2(1, 0) == m01);
+    CHECK(mat2(1, 1) == m10);
 }
 
 TEMPLATE_LIST_TEST_CASE("Accessor operator", "[algebra][matrix][dim2][operator]", FloatingTypes)
 {
-    constexpr TestType x1 = -1.0;
-    constexpr TestType y1 = 3123.77;
-    constexpr TestType x2 = 55.12;
-    constexpr TestType y2 = -10.0001;
+    constexpr TestType m00 = -1.0;
+    constexpr TestType m01 = 3123.77;
+    constexpr TestType m10 = 55.12;
+    constexpr TestType m11 = -10.0001;
 
-    constexpr Matrix<TestType, 2, 2> mat1 = { x1, y1, x2, y2 };
+    constexpr Matrix<TestType, 2, 2> mat1 = { m00, m01, m10, m11 };
 
-    static_assert(mat1(0, 0) == x1);
-    static_assert(mat1(0, 1) == y1);
-    static_assert(mat1(1, 0) == x2);
-    static_assert(mat1(1, 1) == y2);
+    static_assert(mat1(0, 0) == m00);
+    static_assert(mat1(0, 1) == m01);
+    static_assert(mat1(1, 0) == m10);
+    static_assert(mat1(1, 1) == m11);
 
-    CHECK(mat1(0, 0) == x1);
-    CHECK(mat1(0, 1) == y1);
-    CHECK(mat1(1, 0) == x2);
-    CHECK(mat1(1, 1) == y2);
+    CHECK(mat1(0, 0) == m00);
+    CHECK(mat1(0, 1) == m01);
+    CHECK(mat1(1, 0) == m10);
+    CHECK(mat1(1, 1) == m11);
 
     Matrix<TestType, 2, 2> mat2;
-    mat2(0, 0) = y2;
-    mat2(0, 1) = x1;
-    mat2(1, 0) = y1;
-    mat2(1, 1) = x2;
+    mat2(0, 0) = m11;
+    mat2(0, 1) = m00;
+    mat2(1, 0) = m01;
+    mat2(1, 1) = m10;
 
-    CHECK(mat2(0, 0) == y2);
-    CHECK(mat2(0, 1) == x1);
-    CHECK(mat2(1, 0) == y1);
-    CHECK(mat2(1, 1) == x2);
+    CHECK(mat2(0, 0) == m11);
+    CHECK(mat2(0, 1) == m00);
+    CHECK(mat2(1, 0) == m01);
+    CHECK(mat2(1, 1) == m10);
 }
 
 TEMPLATE_LIST_TEST_CASE("Default constructor", "[algebra][matrix][dim2][constructor]", IntegerTypes)
@@ -143,12 +143,12 @@ TEMPLATE_LIST_TEST_CASE("Constructor with diagonal coefficient", "[algebra][matr
 
 TEMPLATE_LIST_TEST_CASE("Copy operator", "[algebra][matrix][dim2][operator]", IntegerTypes)
 {
-    constexpr TestType x1 = 1;
-    constexpr TestType y1 = 4;
-    constexpr TestType x2 = 27;
-    constexpr TestType y2 = 256;
+    constexpr TestType m00 = 1;
+    constexpr TestType m01 = 4;
+    constexpr TestType m10 = 27;
+    constexpr TestType m11 = 256;
 
-    constexpr Matrix<TestType, 2, 2> mat = { x1, y1, x2, y2 };
+    constexpr Matrix<TestType, 2, 2> mat = { m00, m01, m10, m11 };
 
     constexpr auto copy1(mat);
 
@@ -177,12 +177,12 @@ TEMPLATE_LIST_TEST_CASE("Copy operator", "[algebra][matrix][dim2][operator]", In
 
 TEMPLATE_LIST_TEST_CASE("Copy operator", "[algebra][matrix][dim2][operator]", FloatingTypes)
 {
-    constexpr TestType x1 = -18.1;
-    constexpr TestType y1 = 19.2;
-    constexpr TestType x2 = 20.3;
-    constexpr TestType y2 = -21.4;
+    constexpr TestType m00 = -18.1;
+    constexpr TestType m01 = 19.2;
+    constexpr TestType m10 = 20.3;
+    constexpr TestType m11 = -21.4;
 
-    constexpr Matrix<TestType, 2, 2> mat = { x1, y1, x2, y2 };
+    constexpr Matrix<TestType, 2, 2> mat = { m00, m01, m10, m11 };
 
     constexpr auto copy1(mat);
 
@@ -211,78 +211,78 @@ TEMPLATE_LIST_TEST_CASE("Copy operator", "[algebra][matrix][dim2][operator]", Fl
 
 TEMPLATE_LIST_TEST_CASE("Move operator", "[algebra][matrix][dim2][operator]", IntegerTypes)
 {
-    constexpr TestType x1 = 1;
-    constexpr TestType y1 = 4;
-    constexpr TestType x2 = 27;
-    constexpr TestType y2 = 256;
+    constexpr TestType m00 = 1;
+    constexpr TestType m01 = 4;
+    constexpr TestType m10 = 27;
+    constexpr TestType m11 = 256;
 
-    constexpr auto copy1(Matrix<TestType, 2, 2>({ x1, y1, x2, y2 }));
+    constexpr auto copy1(Matrix<TestType, 2, 2>({ m00, m01, m10, m11 }));
 
-    static_assert(copy1(0, 0) == x1);
-    static_assert(copy1(0, 1) == y1);
-    static_assert(copy1(1, 0) == x2);
-    static_assert(copy1(1, 1) == y2);
+    static_assert(copy1(0, 0) == m00);
+    static_assert(copy1(0, 1) == m01);
+    static_assert(copy1(1, 0) == m10);
+    static_assert(copy1(1, 1) == m11);
 
-    CHECK(copy1(0, 0) == x1);
-    CHECK(copy1(0, 1) == y1);
-    CHECK(copy1(1, 0) == x2);
-    CHECK(copy1(1, 1) == y2);
+    CHECK(copy1(0, 0) == m00);
+    CHECK(copy1(0, 1) == m01);
+    CHECK(copy1(1, 0) == m10);
+    CHECK(copy1(1, 1) == m11);
 
-    constexpr auto copy2 = Matrix<TestType, 2, 2>({ x1, y1, x2, y2 });
+    constexpr auto copy2 = Matrix<TestType, 2, 2>({ m00, m01, m10, m11 });
 
-    static_assert(copy2(0, 0) == x1);
-    static_assert(copy2(0, 1) == y1);
-    static_assert(copy2(1, 0) == x2);
-    static_assert(copy2(1, 1) == y2);
+    static_assert(copy2(0, 0) == m00);
+    static_assert(copy2(0, 1) == m01);
+    static_assert(copy2(1, 0) == m10);
+    static_assert(copy2(1, 1) == m11);
 
-    CHECK(copy2(0, 0) == x1);
-    CHECK(copy2(0, 1) == y1);
-    CHECK(copy2(1, 0) == x2);
-    CHECK(copy2(1, 1) == y2);
+    CHECK(copy2(0, 0) == m00);
+    CHECK(copy2(0, 1) == m01);
+    CHECK(copy2(1, 0) == m10);
+    CHECK(copy2(1, 1) == m11);
 }
 
 TEMPLATE_LIST_TEST_CASE("Move operator", "[algebra][matrix][dim2][operator]", FloatingTypes)
 {
-    constexpr TestType x1 = -18.1;
-    constexpr TestType y1 = 19.2;
-    constexpr TestType x2 = 20.3;
-    constexpr TestType y2 = -21.4;
+    constexpr TestType m00 = -18.1;
+    constexpr TestType m01 = 19.2;
+    constexpr TestType m10 = 20.3;
+    constexpr TestType m11 = -21.4;
 
-    constexpr auto copy1(Matrix<TestType, 2, 2>({ x1, y1, x2, y2 }));
+    constexpr auto copy1(Matrix<TestType, 2, 2>({ m00, m01, m10, m11 }));
 
-    static_assert(copy1(0, 0) == x1);
-    static_assert(copy1(0, 1) == y1);
-    static_assert(copy1(1, 0) == x2);
-    static_assert(copy1(1, 1) == y2);
+    static_assert(copy1(0, 0) == m00);
+    static_assert(copy1(0, 1) == m01);
+    static_assert(copy1(1, 0) == m10);
+    static_assert(copy1(1, 1) == m11);
 
-    CHECK(copy1(0, 0) == x1);
-    CHECK(copy1(0, 1) == y1);
-    CHECK(copy1(1, 0) == x2);
-    CHECK(copy1(1, 1) == y2);
+    CHECK(copy1(0, 0) == m00);
+    CHECK(copy1(0, 1) == m01);
+    CHECK(copy1(1, 0) == m10);
+    CHECK(copy1(1, 1) == m11);
 
-    constexpr auto copy2 = Matrix<TestType, 2, 2>({ x1, y1, x2, y2 });
+    constexpr auto copy2 = Matrix<TestType, 2, 2>({ m00, m01, m10, m11 });
 
-    static_assert(copy2(0, 0) == x1);
-    static_assert(copy2(0, 1) == y1);
-    static_assert(copy2(1, 0) == x2);
-    static_assert(copy2(1, 1) == y2);
+    static_assert(copy2(0, 0) == m00);
+    static_assert(copy2(0, 1) == m01);
+    static_assert(copy2(1, 0) == m10);
+    static_assert(copy2(1, 1) == m11);
 
-    CHECK(copy2(0, 0) == x1);
-    CHECK(copy2(0, 1) == y1);
-    CHECK(copy2(1, 0) == x2);
-    CHECK(copy2(1, 1) == y2);
+    CHECK(copy2(0, 0) == m00);
+    CHECK(copy2(0, 1) == m01);
+    CHECK(copy2(1, 0) == m10);
+    CHECK(copy2(1, 1) == m11);
 }
 
-TEMPLATE_LIST_TEST_CASE("Comparision operator", "[algebra][matrix][dim2][constructor]", IntegerTypes)
+TEMPLATE_LIST_TEST_CASE("Comparision operator", "[algebra][matrix][dim2][operator]", IntegerTypes)
 {
-    constexpr TestType x1 = 321;
-    constexpr TestType y1 = 654;
-    constexpr TestType x2 = 987;
-    constexpr TestType y2 = -741;
+    constexpr TestType m00 = 321;
+    constexpr TestType m01 = 654;
+    constexpr TestType m10 = 987;
+    constexpr TestType m11 = -741;
 
-    constexpr Matrix<TestType, 2, 2> mat       = { x1, y1, x2, y2 };
-    constexpr Matrix<TestType, 2, 2> identical = { x1, y1, x2, y2 };
-    constexpr Matrix<TestType, 2, 2> different = { x1, y2, x2, y1 };
+    constexpr Matrix<TestType, 2, 2> mat       = { m00, m01, m10, m11 };
+    constexpr Matrix<TestType, 2, 2> identical = { m00, m01, m10, m11 };
+    constexpr Matrix<TestType, 2, 2> different = { m00, m11, m10, m01 };
 
     static_assert(mat == identical);
     static_assert(mat != different);
@@ -291,16 +291,16 @@ TEMPLATE_LIST_TEST_CASE("Comparision operator", "[algebra][matrix][dim2][constru
     CHECK(mat != different);
 }
 
-TEMPLATE_LIST_TEST_CASE("Comparision operator", "[algebra][matrix][dim2][constructor]", FloatingTypes)
+TEMPLATE_LIST_TEST_CASE("Comparision operator", "[algebra][matrix][dim2][operator]", FloatingTypes)
 {
-    constexpr TestType x1 = -0.1;
-    constexpr TestType y1 = 0.2;
-    constexpr TestType x2 = 0.3;
-    constexpr TestType y2 = -0.4;
+    constexpr TestType m00 = -0.1;
+    constexpr TestType m01 = 0.2;
+    constexpr TestType m10 = 0.3;
+    constexpr TestType m11 = -0.4;
 
-    constexpr Matrix<TestType, 2, 2> mat       = { x1, y1, x2, y2 };
-    constexpr Matrix<TestType, 2, 2> identical = { x1, y1, x2, y2 };
-    constexpr Matrix<TestType, 2, 2> different = { x1, y2, x2, y1 };
+    constexpr Matrix<TestType, 2, 2> mat       = { m00, m01, m10, m11 };
+    constexpr Matrix<TestType, 2, 2> identical = { m00, m01, m10, m11 };
+    constexpr Matrix<TestType, 2, 2> different = { m00, m11, m10, m01 };
 
     static_assert(mat == identical);
     static_assert(mat != different);
