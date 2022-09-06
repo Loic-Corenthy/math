73252e2ce126ecd8d8019e45a2fbfb3a8547456f
Loïc Corenthy <loic.corenthy@gmail.com>
2022-09-06 07:54:43 +0100

    WIP Add scalar matrix operator

--- algebra/include/algebra/Matrix.hpp
+++ algebra/include/algebra/Matrix.hpp
@@ -13,24 +13,24 @@ namespace LCNS::Algebra
     {
     public:
         /*!
-         * \brief Default constructor.
+         * @brief Default constructor.
          */
         constexpr Matrix() = default;
 
         /*!
-         * \brief Constructor with scalar parameter.
+         * @brief Constructor with scalar parameter.
          * @param scalar is the scalar value that will be set to the diagonal coefficients
          */
         constexpr explicit Matrix(auto scalar);
 
         /*!
-         * \brief Constructor with initializer list parameter.
+         * @brief Constructor with initializer list parameter.
          * @param coeffs contains the coefficients of the matrix, row by row
          */
         constexpr Matrix(const std::initializer_list<coordinate>& coeffs);
 
         /*!
-         * \brief Accessor (read/write)
+         * @brief Accessor (read/write)
          * @param i is the index of the row where to find the coefficient to access
          * @param j is the index of the column where to find the coefficient to access
          * @return a reference on the corresponding coefficient
@@ -38,7 +38,7 @@ namespace LCNS::Algebra
         coordinate& operator()(unsigned int i, unsigned int j);
 
         /*!
-         * \brief Accessor (read only)
+         * @brief Accessor (read only)
          * @param i is the index of the row where to find the coefficient to access
          * @param j is the index of the column where to find the coefficient to access
          * @return the corresponding coefficient
@@ -46,29 +46,45 @@ namespace LCNS::Algebra
         constexpr coordinate operator()(unsigned int i, unsigned int j) const;
 
         /*!
-         * \brief Comparision operator
+         * @brief Comparision operator
          * @param rhs is the matrix to compare coefficients from
          * @return true if all coordinates of this matrix and rhs are equal
          */
         constexpr bool operator==(const Matrix<coordinate, rows, cols>& rhs) const;
 
         /*!
-         * \brief Addition operator. Do not modify this object.
+         * @brief Addition operator. Do not modify this object.
          * @param rhs is the matrix to add to this one
          * @return a matrix corresponding to the addition: this + rhs
          */
         constexpr Matrix<coordinate, rows, cols> operator+(const Matrix<coordinate, rows, cols>& rhs) const;
 
         /*!
-         * \brief Substraction operator. Do not modify this object.
+         * @brief Substraction operator. Do not modify this object.
          * @param rhs is the matrix to substract from this one
          * @return a matrix corresponding to the substraction: this - rhs
          */
         constexpr Matrix<coordinate, rows, cols> operator-(const Matrix<coordinate, rows, cols>& rhs) const;
 
+        /*!
+         * @brief Multiplication by a scalar operator
+         * @param scalar is the scalar value that will multiply all the coefficients
+         * @return a reference on this object after the operation
+         */
+        Matrix<coordinate, rows, cols>& operator*=(auto scalar);
+
+        /*!
+         * @brief Division by a scalar operator
+         * @param scalar is the scalar value from which all the coefficients will be divided
+         * @return a reference on this object after the operation
+         */
+        Matrix<coordinate, rows, cols>& operator/=(auto scalar);
+
+        /*! Other operations are achieved using an external operator, see below */
+
     private:
         /*!
-         * \brief Unnamed helper function to get the 1D equalent of i and j
+         * @brief Unnamed helper function to get the 1D equalent of i and j
          * @param i is the index of the row where to find the coefficient to access
          * @param j is the index of the column where to find the coefficient to access
          * @return the equivalent index to access the coefficient in _coeff
@@ -161,7 +177,7 @@ namespace LCNS::Algebra
     {
         if (_rows != rhs._rows || _cols != rhs._cols)
         {
-            throw std::runtime_error("substraction requires matrices of equal dimentions");
+            throw std::runtime_error("Substraction requires matrices of equal dimentions");
         }
 
         auto result = *this;
@@ -172,6 +188,37 @@ namespace LCNS::Algebra
         }
 
         return result;
+    }
+
+    template <Coordinate coordinate, unsigned int rows, unsigned int cols>
+    Matrix<coordinate, rows, cols>& Matrix<coordinate, rows, cols>::operator*=(auto scalar)
+    {
+        static_assert(std::is_same_v<decltype(scalar), coordinate>);
+
+        for (auto& c : _coeff)
+        {
+            c *= scalar;
+        }
+
+        return *this;
+    }
+
+    template <Coordinate coordinate, unsigned int rows, unsigned int cols>
+    Matrix<coordinate, rows, cols>& Matrix<coordinate, rows, cols>::operator/=(auto scalar)
+    {
+        static_assert(std::is_same_v<decltype(scalar), coordinate>);
+
+        if (scalar == static_cast<coordinate>(0))
+        {
+            throw std::runtime_error("Divide by zero");
+        }
+
+        for (auto& c : _coeff)
+        {
+            c /= scalar;
+        }
+
+        return *this;
     }
 
     template <Coordinate coordinate, unsigned int rows, unsigned int cols>
@@ -198,4 +245,46 @@ namespace LCNS::Algebra
 
         return result;
     }
+
+    template <Coordinate coordinate, unsigned int rows, unsigned int cols>
+    constexpr Matrix<coordinate, rows, cols> operator*(Matrix<coordinate, rows, cols>& lhs, auto rhs)
+    {
+        static_assert(std::is_same_v<decltype(rhs), coordinate>);
+
+        for (size_t i = 0; i < rows; ++i)
+            for (size_t j = 0; j < cols; ++j)
+                lhs(i, j) *= rhs;
+
+        return lhs;
+    }
+
+    template <Coordinate coordinate, unsigned int rows, unsigned int cols>
+    constexpr Matrix<coordinate, rows, cols> operator*(auto lhs, Matrix<coordinate, rows, cols>& rhs)
+    {
+        static_assert(std::is_same_v<decltype(lhs), coordinate>);
+
+        for (size_t i = 0; i < rows; ++i)
+            for (size_t j = 0; j < cols; ++j)
+                rhs(i, j) *= lhs;
+
+        return rhs;
+    }
+
+    template <Coordinate coordinate, unsigned int rows, unsigned int cols>
+    constexpr Matrix<coordinate, rows, cols> operator/(Matrix<coordinate, rows, cols>& lhs, auto rhs)
+    {
+        static_assert(std::is_same_v<decltype(rhs), coordinate>);
+
+        if (scalar == static_cast<coordinate>(0))
+        {
+            throw std::runtime_error("Divide by zero");
+        }
+
+        for (size_t i = 0; i < rows; ++i)
+            for (size_t j = 0; j < cols; ++j)
+                lhs(i, j) /= rhs;
+
+        return lhs;
+    }
+
 }  // namespace LCNS::Algebra
