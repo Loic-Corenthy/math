ca1bbae6324ed60a504622ca2436006f6a52c281
Loïc Corenthy <loic.corenthy@gmail.com>
2022-09-20 00:20:08 +0100

    Adjust epsilon accuracy

--- algebra/tests/Helper.hpp
+++ algebra/tests/Helper.hpp
@@ -24,7 +24,7 @@ namespace LCNS
     {
         if constexpr (std::is_same_v<T, float>)
         {
-            return 0.000'001f;
+            return 0.000'01f;
         }
         else if constexpr (std::is_same_v<T, double>)
         {
@@ -32,7 +32,7 @@ namespace LCNS
         }
         else if constexpr (std::is_same_v<T, long double>)
         {
-            return 0.000'000'000'000'001;
+            return 0.000'000'000'001;
         }
     }
 }  // namespace LCNS
