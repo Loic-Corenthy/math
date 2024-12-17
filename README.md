b714d974bc8ffb4829f3bf78cdcc695cd3104311
Loïc Corenthy <loic.corenthy@gmail.com>
2024-12-17 00:43:52 +0000

    Update benchmarks

--- README.md
+++ README.md
@@ -18,3 +18,21 @@ Templated classes to accommodate with different types and sizes.
 
 * **combinatorics**
     - Permutation generator function
+
+
+* **large**
+
+| **Benchmark Name**               | **Samples** | **Mean**  | **Std Dev** |
+|----------------------------------|-------------|-----------|-------------|
+| **Simd multiplication**          | 100         | 3.36515 s | 44.8839 ms  |
+| **Multithreading multiplication**| 100         | 2.88052 s | 67.1787 ms  |
+
+Simple benchmark measuring the time to multiply two matrices A and B (of float numbers) with the following dimensions:
+
+- A = 1000 rows x 1200 cols
+- B = 1200 rows x 1100 cols
+
+The test was performed on a Intel(R) Core(TM) i9-9820X CPU.
+
+(See `./large/tests/BenchMarkLargeMatrix.cpp` for more details)
+
--- large/tests/BenchmarkLargeMatrix.cpp
+++ large/tests/BenchmarkLargeMatrix.cpp
@@ -19,26 +19,26 @@ using namespace std;
 class BenchmarkFixture
 {
 public:
-    BenchmarkFixture()
-    : lhs(256, 512, true)
-    , rhs(512, 126, true)
-    , gen(rd())
+    BenchmarkFixture(size_t lhs_row_count, size_t lhs_col_count, size_t rhs_row_count, size_t rhs_col_count)
+    : lhs(lhs_row_count, lhs_col_count, true)
+    , rhs(rhs_row_count, rhs_col_count, true)
+    , _gen(_rd())
     {
         cout << "CTEST_FULL_OUTPUT\n";
     }
 
     int rand_index_row()
     {
-        static std::uniform_int_distribution<> dis(0, 255);
+        static std::uniform_int_distribution<> dis(0, lhs.row_count() - 1);
 
-        return dis(gen);
+        return dis(_gen);
     }
 
     int rand_index_col()
     {
-        static std::uniform_int_distribution<> dis(0, 126);
+        static std::uniform_int_distribution<> dis(0, rhs.col_count() - 1);
 
-        return dis(gen);
+        return dis(_gen);
     }
 
 protected:
@@ -46,16 +46,31 @@ protected:
     Matrix rhs;
 
 private:
-    std::random_device rd;
-    std::mt19937       gen;
+    random_device _rd;
+    mt19937       _gen;
 };
 
-
-TEST_CASE_METHOD(BenchmarkFixture, "Multiplication", "[benchmark][matrix][large]")
+struct BenchmarkFixture256 : public BenchmarkFixture
 {
-    Matrix res1(256, 126);
-    Matrix res2(256, 126);
-    Matrix res3(256, 126);
+    BenchmarkFixture256()
+    : BenchmarkFixture(256, 512, 512, 126)
+    {
+    }
+};
+
+struct BenchmarkFixture1000 : public BenchmarkFixture
+{
+    BenchmarkFixture1000()
+    : BenchmarkFixture(1000, 1200, 1200, 1100)
+    {
+    }
+};
+
+TEST_CASE_METHOD(BenchmarkFixture256, "Multiplication", "[benchmark][matrix][large][256]")
+{
+    Matrix res1(lhs.row_count(), rhs.col_count());
+    Matrix res2(lhs.row_count(), rhs.col_count());
+    Matrix res3(lhs.row_count(), rhs.col_count());
 
     vector<float> test_data1;
     vector<float> test_data2;
@@ -88,4 +103,25 @@ TEST_CASE_METHOD(BenchmarkFixture, "Multiplication", "[benchmark][matrix][large]
         CHECK_THAT(res1(row_idx, col_idx), WithinAbs(res2(row_idx, col_idx), 1e-3));
         CHECK_THAT(res1(row_idx, col_idx), WithinAbs(res3(row_idx, col_idx), 1e-3));
     }
+}
+
+
+TEST_CASE_METHOD(BenchmarkFixture1000, "Multiplication", "[benchmark][matrix][large][1000]")
+{
+    BENCHMARK("Simd multiplication")
+    {
+        const auto res = multiply_simd(lhs, rhs);
+
+        cout << res(345, 567) << '\n';
+        return 0;
+    };
+
+    BENCHMARK("Multithreading multiplication")
+    {
+        const auto res = multiply_concurrently(lhs, rhs);
+
+        cout << res(345, 567) << '\n';
+
+        return 0;
+    };
 }
