6431aa4b27ecee2d4bfa3baed2d6c4f0a76eb09f
Loïc Corenthy <loic.corenthy@gmail.com>
2025-11-20 23:31:44 +0000

    Add more compile options to cover all instruction sets

--- CMake/CheckCPU.cmake
+++ CMake/CheckCPU.cmake
@@ -19,27 +19,21 @@ else()
 endif()
 
 # AVX512 Test
-# set(AVX512_TEST_SOURCE "
-# #include <immintrin.h>
-# int main() {
-#     __m512 v = _mm512_set1_ps(1.0f);
-#     return 0;
-# }
-# ")
+set(AVX512_TEST_SOURCE "
+#include <immintrin.h>
+int main() {
+    __m512 v = _mm512_set1_ps(1.0f);
+    return 0;
+}
+")
 
-# set(CMAKE_REQUIRED_FLAGS -mavx512f)
-# check_cxx_source_compiles("${AVX512_TEST_SOURCE}" AVX512_ENABLED_ON_CPU)
+set(CMAKE_REQUIRED_FLAGS -mavx512f)
+check_cxx_source_compiles("${AVX512_TEST_SOURCE}" AVX512_ENABLED_ON_CPU)
 
-# if (AVX512_ENABLED_ON_CPU)
-#     message(STATUS "AVX512 instructions supported")
-# else()
-#     message(STATUS "AVX512 instructions not supported")
-# endif()
+if (AVX512_ENABLED_ON_CPU)
+    message(STATUS "AVX512 instructions supported")
+else()
+    message(STATUS "AVX512 instructions not supported")
+endif()
 
 unset(CMAKE_REQUIRED_FLAGS)
-
-# # Check for a compatible compiler and add the option.
-# if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-#     # Wrap the option in "SHELL:" to ensure it is treated as a single argument.
-#     target_compile_options(my_executable PRIVATE "SHELL:-march=native")
-# endif()
