356b0bdcefa051be25ab40e87aa01b3a8536dac5
Loïc Corenthy <loic.corenthy@gmail.com>
2026-09-01 00:31:26 +0100

    WIP

--- Scripts/modified_scripts.py
+++ Scripts/modified_scripts.py
@@ -1,3 +1,5 @@
+import jq
+import json
 import logging
 import subprocess
 import sys
@@ -50,13 +52,35 @@ def ModifiedFiles(build_dir : str):
 
     # logger.info(f"Da result is {codemodel_json[0]}")
 
+    # Get all matching elements as a list
+
+    logger.debug("=======================================================================")
+    logger.debug("=======================================================================")
+    logger.debug(codemodel_json[0])
+    logger.debug("=======================================================================")
+    logger.debug("=======================================================================")
+
+    with open(codemodel_json[0], "r") as f:
+        data = json.load(f)
+
+    # results = jq.all('.configurations[0] | (.targets[],.abstractTargets[]?) | select(.directoryIndex!=0) | .jsonFile', codemodel_json[0])
+    results = jq.all('.[]', data)
+    print(results)
+    # Output: [{'name': 'production-db', 'type': 'database'}, {'name': 'production-web', 'type': 'server'}]
+
+    # Get only the first matching element
+    # first_match = jq.first('.[] | select(.type == "server")', data)
+    # print(first_match)
+    # Output: {'name': 'staging-web', 'type': 'server'}
+
+
 
     # 4. Extract target JSON references from the codemodel
     # Remove directoryIndex = 0 because we use FetchContent_Declare from the root directory and we don't want to take these targets into account
-    targets_request = f"jq -r '.configurations[0] | (.targets[],.abstractTargets[]?) | select(.directoryIndex!=0) | .jsonFile' {codemodel_json[0]}"
-    target_json = RunRequest(targets_request)
-
-    logger.debug(f"The json targets are {target_json}")
+    # targets_request = f"jq -r '.configurations[0] | (.targets[],.abstractTargets[]?) | select(.directoryIndex!=0) | .jsonFile' {codemodel_json[0]}"
+    # target_json = RunRequest(targets_request)
+    #
+    # logger.debug(f"The json targets are {target_json}")
 
     # Test outputs
     # logger.debug("This is a debug message.")
